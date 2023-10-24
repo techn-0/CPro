a5436a54a534795c0e010e5eddd859d06d292c01
Alisser <gnlrjswjd@naver.com>
2023-10-24 19:54:50 +0900

    ?

--- 반복문/6.c
+++ 반복문/6.c
@@ -3,9 +3,9 @@
 int main(void)
 {
     int n = 100;
-    for (int i = 0; i <= n; i++)
+    for (int i = 1; i <= n; i++)
     {
-        if (i > 0 && i % 3 == 0)
+        if (i % 3 == 0 && i % 5 == 0)
             printf("%d ", i);
     }
 }
